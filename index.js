062417ca99df0441ed2bb0c29b25c3b887a53b21
Nurane2003 <nurane.sirinova.2003@gmail.com>
2024-03-07 01:35:00 +0400

    added new file

---
+++ index.js
@@ -0,0 +1,32 @@
+var brands=["Acer","Apple","Sony","Samsung"]
+var heroes=["Black Panther","Cyborg","Black Canary","Donna Troy","Huntress","Blue Beetle","Captain Atom"]
+var books=["Calculus Early Transcendentals","Ravens","The Self Illusion","Harry Potter"]
+var text=["Laptop","Beanbag","Cats","Slippers"]
+var howIfeel=["Sleep Deprived","Wired on Coffee","Excited"]
+
+
+
+for(var i=0; i<brands.length; i++){
+    console.log(brands[i]);
+}
+console.log("---------");
+
+for(var a=0; a<heroes.length; a++){
+    console.log(heroes[a]);
+}
+console.log("---------");
+
+for(var d=0; d<books.length; d++){
+    console.log(books[d]);
+}
+console.log("---------");
+
+for(var f=0; f<text.length; f++){
+    console.log(text[f]);
+}
+console.log("---------");
+
+for(var r=0; r<howIfeel.length; r++){
+    console.log(howIfeel[r]);
+}
+console.log("---------");
